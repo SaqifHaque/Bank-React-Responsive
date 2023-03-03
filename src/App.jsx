d96109444bb0acb64f28cbf352ef08acf501ea23
Saqif <saqifhaque@gmail.com>
2023-03-03 22:03:18 +0600

    adding component on Home layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import styles from './style';
+import styles from './styles';
 
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
-function App() {
+const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-            Navbar
+            <Navbar />
         </div>
       </div>
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
       <div className={`bg-primary ${styles.padingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
+          <Stats />
+          <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
         </div>
       </div>
     </div>
