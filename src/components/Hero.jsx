6b2af26a643690f7750e9b5af323e0eabe02f51c
Saqif <saqifhaque@gmail.com>
2023-03-04 23:30:36 +0600

    working on hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import React from 'react';
+import styles from '../styles';
+import { discount, robot } from '../assets';
+ 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className={`flex flex-col md:flex-row ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16`}>
+        <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
+          
+        </div>
+      </div>
+
+    </section>
   )
 }
 
