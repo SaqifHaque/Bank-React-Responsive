05b1c99f9fd7d70c9f8d43074c2ed74d6ba13db8
Saqif <saqifhaque@gmail.com>
2023-03-05 22:41:46 +0600

    code refactor and bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { close, logo, menu } from '../assets';
 import { navLinks } from '../constants';
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav className="bg-primary w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hawkpay" className='w-[124px] h-[32px]'/>
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
-        {navlinks.map((nav, index) => {
+        {navLinks.map((nav, index) => {
           <li
           key={nav.id}
           className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}>
@@ -21,10 +21,10 @@ const Navbar = () => {
       </ul>
 
       <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img src={toggle ? close : menu} alt="menu" classname="w-[28px] h-[28px] object-contain" onClick={() => setToggle((prev) => !prev)}/>
+        <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain" onClick={() => setToggle((prev) => !prev)}/>
         <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
         <ul className='list-none sm:flex flex-col justify-end items-center flex-1'>
-        {navlinks.map((nav, index) => {
+        {navLinks.map((nav, index) => {
           <li
           key={nav.id}
           className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}>
