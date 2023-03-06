316d0b84c32fdf6f2128f52e99d7f1c6ead6a61b
Saqif <saqifhaque@gmail.com>
2023-03-06 21:12:01 +0600

    gradient update and padding fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,21 @@ const App = () => {
             <Navbar />
         </div>
       </div>
-      <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Hero />
         </div>
       </div>
-      <div className={`bg-primary ${styles.padingX} ${styles.flexStart}`}>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Stats />
           <Business />
-        <Billing />
-        <CardDeal />
-        <Testimonials />
-        <Clients />
-        <CTA />
-        <Footer />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
     </div>
