58493458767019d1174e3b721304e974bbd8a12b
Saqif <saqifhaque@gmail.com>
2023-03-22 23:09:11 +0600

    Ui update

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,7 +6,7 @@ import FeedbackCard from './FeedbackCard';
 const Testimonials = () => {
   return (
     <section id="clients" className={`${styles.padding} ${styles.flexCeter} flex-col relative`}>
-      <div/>
+      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient"/>
         
       <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
         <h1 className={styles.heading2}>What people are <br className="sm:block  hiddren"/>
